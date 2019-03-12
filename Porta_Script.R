8b7056ca49a76b612107fc5923a54966266d5590
Ignacio Sanchez <nachospt@hotmail.com>
2019-03-12 13:58:55 +0100

    Initial commit

---
+++ Porta_Script.R
@@ -0,0 +1,14 @@
+################################### Portabilidad Shiny ###################################
+
+## 1.Preliminar steps
+  # 1.1 Data Load
+  # 1.2 Library Load
+## 2.Data Load
+##
+
+# 1.1 Data Load
+setwd()
+Datos = read.csv("Portabilidty_Set.csv")
+
+# 1.2 Library Load
+library(ggplot2)
