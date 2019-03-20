3cb8181547b5217f667c1c1e96c5f21ddf7edd21
Ignacio Sanchez <nachospt@hotmail.com>
2019-03-20 17:56:04 +0100

    Shiny app v3
    Implemented reactivity of elements.

--- server.R
+++ server.R
@@ -1,4 +1,7 @@
 server <- function(input, output) {
+  Upd.Porta.1 <- reactive({filter(Porta.1, Operador.Grupo == input$Operator & ano.mes >= input$MinYear)}) 
+  Upd.Porta.2 <- reactive({filter(Porta.2, Operador.Grupo == input$Operator & ano.mes >= input$MinYear)}) 
+  
   filetype = "csv"
   output$download_data <- downloadHandler(
     filename = function() {
@@ -6,21 +9,25 @@ server <- function(input, output) {
     },
     content = function(file) {
       if(filetype == "csv"){
-        write_csv(Porta.1, path = file)
+        write_csv(Upd.Porta.1(), path = file)
       }
     }
   )
   
+  output$description <- renderText({
+    paste0(nrow(Upd.Porta.1()))})
+  
   # Create scatterplot object the plotOutput function is expecting
   output$scatterplot <- renderPlot({
-    ggplot(data = Porta.2[itemstoplot,],
+    ggplot(data = Upd.Porta.2(),
            aes(y = Importaciones, x = Exportaciones)) +
-      geom_polygon(data = zones, aes(y = Importaciones, x = Exportaciones, group = group), alpha = 0.5, fill = zones$color, color = zones$color, linetype = 0, inherit.aes = FALSE) +
-      geom_point(size=2, aes(colour=Donante.Grupo)) +
+      geom_polygon(data = zones, aes(y = Importaciones, x = Exportaciones, group = as.factor(group)), alpha = 0.5, fill = zones$color, color = zones$color, linetype = 0, inherit.aes = FALSE) +
+      geom_point(size = 2, aes(colour = Donante.Grupo)) +
       geom_line(aes(color = Donante.Grupo), arrow = arrow(length=unit(0.30,"cm"), type = "closed")) +
-      geom_text(aes(label = Porta.2[itemstoplot,"ano.mes"], y = Importaciones + 5000), size = 3)
+      geom_text(aes(label = Upd.Porta.2()[,"ano.mes"], y = Importaciones + 5000), size = 3, color = "grey29") +
+      scale_color_manual(values = sapply(levels(as.factor(Upd.Porta.2()[, "Donante.Grupo"])), function(x) switch(x, "Vodafone" = "#E60000", "Movistar" = "#00B6E8", "Masmovil" = "#FFE500", "Orange" = "#FF9800", "Resto" = "#01B8AA")))
   })
   
   # Create rawtable object the plotOutput function is expecting
-  output$rawtable <- renderTable({Porta.1})
+  output$rawtable <- DT::renderDataTable({DT::datatable(Upd.Porta.1(), options = list(pageLength = 10))})#renderTable({Porta.1})
 }
--- ui.R
+++ ui.R
@@ -7,48 +7,48 @@ ui <- fluidPage(theme = shinytheme("cerulean"),
                 sidebarLayout(
                   
                   # Inputs
-                  sidebarPanel(
-                    
-                    h3("Selection"),      # Third level header: Selection
-                    
-                    # Select Operator
-                    selectInput(inputId = "Operator", 
-                                label = "Operator:",
-                                choices = c("Vodafone", "Movistar", "Orange", "Masmovil", "Resto"),
-                                selected = "Vodafone"
-                    ),
-                    
-                    # Select Years
-                    selectInput(inputId = "MinYear", 
-                                label = "MinimumYear:",
-                                choices = levels(as.factor(Porta.2$ano.mes)),
-                                selected = "1604"
-                    ),
-                    
-                    # Built with Shiny by RStudio
-                    br(),
-                    h5("Built with",
-                       img(src = "https://www.rstudio.com/wp-content/uploads/2014/04/shiny.png", height = "30px"),
-                       "by",
-                       img(src = "https://www.rstudio.com/wp-content/uploads/2014/07/RStudio-Logo-Blue-Gray.png", height = "30px"),
-                       "."),
-                    img(src = "image.png", height = "50px")
+                  sidebarPanel( width = 3,
+                                
+                                h3("Selection"),      # Third level header: Selection
+                                
+                                # Select Operator
+                                selectInput(inputId = "Operator", 
+                                            label = "Operator:",
+                                            choices = c("Vodafone", "Movistar", "Orange", "Masmovil", "Resto"),
+                                            selected = "Vodafone"
+                                ),
+                                
+                                # Select Years
+                                selectInput(inputId = "MinYear", 
+                                            label = "MinimumYear:",
+                                            choices = levels(as.factor(Porta.2$ano.mes)),
+                                            selected = "1712"
+                                ),
+                                
+                                # Built with Shiny by RStudio
+                                br(),
+                                h5("Built with",
+                                   img(src = "https://www.rstudio.com/wp-content/uploads/2014/04/shiny.png", height = "30px"),
+                                   "by"),
+                                h5("Ignacio Sánchez Puente"),
+                                img(src = "image.png", height = "50px")
                   ),
                   
                   # Output:
                   mainPanel(tabsetPanel(type = "tabs",
-                                        tabPanel(title = "tab 1",
+                                        tabPanel(title = "Overview",
                                                  h3("Overview"),
                                                  br(),
                                                  HTML("Here goes the content")),
-                                        tabPanel(title = "tab 2",
+                                        tabPanel(title = "Portability Graph",
                                                  h3("Portability graph"),
                                                  br(),
-                                                 plotOutput(outputId = "scatterplot")),
-                                        tabPanel(title = "tab 3",
+                                                 plotOutput(outputId = "scatterplot"),
+                                                 textOutput(outputId = "description")),
+                                        tabPanel(title = "Raw table",
                                                  h3("Raw Table"),
                                                  br(),
-                                                 tableOutput(outputId = "rawtable"),
+                                                 DT::dataTableOutput(outputId = "rawtable"), #tableOutput(outputId = "rawtable"),
                                                  downloadButton(outputId = "download_data", label = "Download data"))
                   )
                   )
